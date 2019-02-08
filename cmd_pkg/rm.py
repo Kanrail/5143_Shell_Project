76f4d4550e8293ab588d541f4cf6c3f38982f740
Daniel B <generalkan@hotmail.com>
2019-02-08 00:07:09 -0600

    Update rm.py
    Added -r and retooled the entire rm command to be more code efficient.

--- cmd_pkg/rm.py
+++ cmd_pkg/rm.py
@@ -1,5 +1,16 @@
 import sys
 import os
+from shutil import rmtree
+
+def deleteItem(relPath, item, flag):
+    try:
+        if flag=='r': #r modifier for recursive destruction
+            rmtree(relPath+item)
+        else:
+            rmdir(relPath+item)
+    except:
+        os.remove(relPath+item)
+
 
 def rm(**kwargs):
     """
@@ -12,6 +23,9 @@ def rm(**kwargs):
         rm deletes a FILE. FILE must be existent, or function will return an
         error
 
+    OPTIONS
+        -r  Recursively delete directories and files based on paramaters.
+
     EXAMPLE
         rm file.txt
 
@@ -20,41 +34,55 @@ def rm(**kwargs):
         params = kwargs['params']
     if 'path' in kwargs:
         path = kwargs['path']
+    if 'tags' in kwargs:
+        tags = kwargs['tags']
 
     try:
         paramPieces = []
+
         paramCharacters = list(str(params[0]))
+        tagsCharacters = list(str(tags[0]))
+        currentDirectory = os.listdir(path[0])
+        tagFlag = ''
+
+        if 'r' in tagsCharacters:
+            tagFlag = 'r'
         if '*' in paramCharacters: #checks for wildcard
             paramPieces = params[0].split('*') #breaks the string in half around wildcard
-            fileList = []
-            if len(paramPieces)>1: #if the wildcard was in the middle of string somewhere
+            if params[0]=='*':
+                #rmtree(path[0])
                 for root,dirs,files in os.walk(path[0]): #steps through filestructure of path
                     for file in files:
-                        if file.startswith(paramPieces[0]) and file.endswith(paramPieces[1]):
-
                             os.remove(os.path.join(root,file))
+                    for dir in dirs:
+                        if tagFlag == 'r':
+                            rmtree(os.path.relpath(os.path.join(root,dir), "."))
+                        else:
+                            rmdir(os.path.relpath(os.path.join(root,dir), "."))
                 return ''
+            elif '' not in paramPieces: #if the wildcard was in the middle of string somewhere
+                for target in currentDirectory:
+                    if target.startswith(paramPieces[0]) and target.endswith(paramPieces[1]):
+                        deleteItem(path[0], target, tagFlag)
+                return '' #end middle wildcard if statement
 
-            if paramCharacters[0] is '*': #if the first character in params is wildcard
-                for root,dirs,files in os.walk(path[0]):#steps through filestructure of path
-                    for file in files:
-                        if file.endswith(paramPieces[1]):
-                            os.remove(os.path.join(root,file))
-                return ''
+            elif paramPieces[0] is '': #if the first character in params is wildcard
+                for target in currentDirectory:
+                    if target.endswith(paramPieces[1]):
+                        deleteItem(path[0], target, tagFlag)
+                return '' #end first wildcard character statement
 
             else: #if last character in params is wildcard
-                for root,dirs,files in os.walk(path[0]):#steps through filestructure of path
-                    for file in files:
-                        if file.startswith(paramPieces[1]):
-                            os.remove(os.path.join(root,file))
-                return ''
-
-        os.remove(path[0]+params[0])#fires if no wildcard present, just deletes file
+                for target in currentDirectory:
+                    if target.startswith(paramPieces[0]):
+                        deleteItem(path[0], target, tagFlag)
+                return ''#end last wildcard character statement
+        deleteItem(path[0], params[0], tagFlag)#fires if no wildcard present, just deletes file
         return ''
-        
+
     except:
         return 'Invalid Input: No such file or directory\n'
 
 if __name__=='__main__':
-    sys.stdout.write(rm(params=['*txt'],path=['./testdir/']))
+    sys.stdout.write(rm(params=['test*'],path=['./testdir/testdir2/'],tags=['r']))
     pass

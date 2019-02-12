67ff8393bed41e9ddd84b826ae104cc8b00e5a6a
Shenglin Sun <sunshenglin12@gmail.com>
2019-02-12 02:21:10 -0600

    Add files via upload

--- cmd_pkg/ls-l.py
+++ cmd_pkg/ls-l.py
@@ -1,7 +1,17 @@
-from __future__ import print_function
 import os
 
 
-path = os.listdir(os.getcwd())
-for files in path:
-    print(files)
+def ls():
+    try:
+        path  = os.listdir(os.getcwd())
+        for name in path:
+            files  = os.path.join(os.getcwd(), name)
+            inode = os.stat(files)
+            print(str(inode.st_gid) + ' '  + str(inode.st_uid) + ' ' + str(inode.st_size) + ' ' +  str(inode.st_atime) + ' ' + name)
+
+    except:
+        return 'error\n'
+
+if __name__ == '__main__':
+    a=ls()
+    print(a)

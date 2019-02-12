80fcf7368e16883ebc6a47bfa2a4f169398ed335
Kanrail <generalkan@hotmail.com>
2019-02-11 22:46:18 -0600

    Lots of changes and it actually works now, no piped commands, but it works. Still need to debug a few of the individual commands though.

--- shell.py
+++ shell.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import threading
 import sys
+import os
 from cmd_pkg import commands
+import getpass
 
 
 def exit(**kwargs):
@@ -11,9 +13,21 @@ def exit(**kwargs):
 class CommandHelper(object):
     def __init__(self):
         self.commands = {}
-        self.commands['ls'] = ls
-        self.commands['cat'] = cat
-        self.commands['pwd'] = pwd
+        self.commands['ls'] = commands.ls
+        self.commands['cat'] = commands.cat
+        self.commands['pwd'] = commands.pwd
+        self.commands['cd'] = commands.cd
+        self.commands['cp'] = commands.cp
+        self.commands['grep'] = commands.grep
+        self.commands['head'] = commands.head
+        self.commands['mkdir'] = commands.mkdir
+        self.commands['mv'] = commands.mv
+        self.commands['rm'] = commands.rm
+        self.commands['rmdir'] = commands.rmdir
+        self.commands['tail'] = commands.tail
+        self.commands['wc'] = commands.wc
+        self.commands['who'] = commands.who
+        self.commands['sort'] = commands.sort
         self.commands['exit'] = exit
 
     def invoke(self, **kwargs):
@@ -34,7 +48,15 @@ class CommandHelper(object):
 
         if 'tags' in kwargs:
             tags = kwargs['tags']
+        else:
+            tags = []
 
+        if 'path' in kwargs:
+            path = kwargs['path']
+        else:
+            path = []
+        return self.commands[cmd](params=params,path=path,tags=tags)
+        '''
         # One way to invoke using dictionary
         if not thread:
             self.commands[cmd](params=params)
@@ -47,7 +69,7 @@ class CommandHelper(object):
 
             c.start()
             c.join()
-
+        '''
 
     def exists(self, cmd):
         return cmd in self.commands
@@ -59,15 +81,59 @@ if __name__ == '__main__':
 
     while True:
         # get input from terminal (use input if raw_input doesn't work)
-        command_input = raw_input('% ')
+        currentDirectory = os.path.basename(os.getcwd())
+        currentUser = getpass.getuser()
+        command_input = raw_input(currentDirectory+' '+currentUser+'% ')
 
         # remove command from params (very over simplified)
         command_input = command_input.split()
 
-        # tags are any part of the command that star with a '-'
-        for item in command_input:
+        # tags are any part of the command that start with a '-'
+        tags = []
+        path = []
+        for item in command_input[1:]:
             commandPiece = list(item)
-            if
+            if commandPiece[0]=='-':
+                commandPiece.remove('-')
+                if 'n' in commandPiece:
+                    tags.append('n')
+                    hasNumber = False
+                    wholeTagLine = ''.join(commandPiece)
+                    nextCommand = command_input[command_input.index(item)+1]
+                    for char in wholeTagLine:
+                        if char.isdigit(): #if the value for -n is attached without a space
+                            tags.append(''.join(num for num in wholeTagLine if num.isdigit()))
+                            break
+                        '''elif nextCommand.isdigit(): #if there is a space between -n and its value
+                            tags.append(nextCommand)
+                            command_input.remove(nextCommand)
+                            break'''
+                else:
+                    for tag in commandPiece:
+                        tags.append(tag)
+                command_input.remove(item)
+            elif '/' in commandPiece:
+                paramPath = []
+                pathEnd = 0
+                for char in commandPiece:
+                    if char == '/':
+                        pathEnd = commandPiece.index(char)
+                path.append(''.join(commandPiece[0:pathEnd]))
+                #removes the path from the parameter
+                command_input[command_input.index(item)]=''.join(commandPiece[pathEnd+1:])
+            elif item== '..':
+                path.append(item)
+                command_input.pop(command_input.index(item))
+            elif item== '~':
+                path.append(item)
+                command_input.pop(command_input.index(item))
+            elif command_input.index(item)=='|':
+                #Do nothing for now, will get this working later. Ignoring further commands for now
+                command_input = command_input[0:command_input.index(item)-1]
+            else: #if a paremeter has no path given, default a paremeter of local
+                path.append('./')
+
+        # path for every parameter, if none given will insert just a ./
 
         # params are all but first position in list
         params = command_input[1:]
@@ -77,6 +143,20 @@ if __name__ == '__main__':
 
         # if command exists in our shell
         if ch.exists(cmd):
-            ch.invoke(cmd=cmd, params=params,thread=True)
+            #print ('Command is '+str(cmd))
+            #print ('Tags are '+str(tags))
+            #print ('Paths are '+str(path))
+            #print ('Params are '+str(params))
+            returnStatement = ch.invoke(cmd=cmd,tags=tags,path=path,params=params,thread=True)
+            #if cmd == 'cd':
+                #print ('Directory is now: '+os.getcwd())
+            if type(returnStatement) is list:
+                for line in returnStatement:
+                    sys.stdout.write(line)
+                sys.stdout.write('\n')
+            elif type(returnStatement) is str:
+                sys.stdout.write(returnStatement+'\n')
+            else:
+                pass
         else:
             print("Error: command %s doesn't exist." % (cmd))

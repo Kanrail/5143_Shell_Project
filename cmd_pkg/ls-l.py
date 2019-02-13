bc49ef8eaf06366e249b826fd9115e391706dfa2
Shenglin Sun <sunshenglin12@gmail.com>
2019-02-13 02:12:19 -0600

    Add files via upload

--- cmd_pkg/ls-l.py
+++ cmd_pkg/ls-l.py
@@ -1,13 +1,21 @@
 import os
-
-
+import stat
+import datetime
 def ls():
     try:
         path  = os.listdir(os.getcwd())
         for name in path:
             files  = os.path.join(os.getcwd(), name)
             inode = os.stat(files)
-            print(str(inode.st_gid) + ' '  + str(inode.st_uid) + ' ' + str(inode.st_size) + ' ' +  str(inode.st_atime) + ' ' + name)
+            #inf=str(inode.st_mode)
+            #inf=str(stat.filemode(os.stat(files).st_mode))
+            inf=str(inode.st_mode)
+            gid=str(inode.st_gid)
+            uid=str(inode.st_uid)
+            siz=str(inode.st_size)
+            time=str(inode.st_atime)
+            #date=datetime.datetime.fromtimestamp(time)
+            print(inf + ' ' + gid + ' ' + uid + ' '  + time +' '+ siz  + ' ' + name)
 
     except:
         return 'error\n'

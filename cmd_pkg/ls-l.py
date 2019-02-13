864790cafb320d318d68936e16043f0304f6d77e
Shenglin Sun <sunshenglin12@gmail.com>
2019-02-13 15:52:16 -0600

    Add files via upload

--- cmd_pkg/ls-l.py
+++ cmd_pkg/ls-l.py
@@ -1,20 +1,28 @@
 import os
 import stat
-import datetime
+from datetime import datetime
+import time
+from grp import getgrgid
+from pwd import getpwuid
 def ls():
     try:
         path  = os.listdir(os.getcwd())
         for name in path:
             files  = os.path.join(os.getcwd(), name)
             inode = os.stat(files)
-            #inf=str(inode.st_mode)
-            #inf=str(stat.filemode(os.stat(files).st_mode))
             inf=str(inode.st_mode)
-            gid=str(inode.st_gid)
-            uid=str(inode.st_uid)
+            #inf=str(stat.filemode(os.stat(files).st_mode)
+           
+            gid=str(getgrgid(inode.st_gid).gr_name)
+            uid=str(getpwuid(inode.st_uid).pw_name)
             siz=str(inode.st_size)
             time=str(inode.st_atime)
-            #date=datetime.datetime.fromtimestamp(time)
+            #date=datetime.utcfromtimestamp(time).strftime('%Y-%m_d %H:%%M:%S')
+            #date=time.ctime(int(time))
+            #date=datetime.datetime.fromtimestamp(int(time))
+            permission ={'0':('---'),'1':('--x'),'2':('-w-'),'3':('-wx'),'4':('r--'),'5':('rw-'),'7':('rwx')}
+            #inf=str(permission(inode.st_mode))
+
             print(inf + ' ' + gid + ' ' + uid + ' '  + time +' '+ siz  + ' ' + name)
 
     except:

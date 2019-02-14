95b60343567bfd5762a2a90d0b01b84eb32f5e32
Shenglin Sun <sunshenglin12@gmail.com>
2019-02-14 02:51:21 -0600

    Add files via upload

--- cmd_pkg/ls.py
+++ cmd_pkg/ls.py
@@ -1,13 +1,6 @@
 import os
 import glob
 
-
-"""
-
-iner function
-bean value
-
-"""
 def ls-n(path):
     try:
         path = glob.glob(os.path.join('*'))
@@ -42,23 +35,31 @@ def ls-t(path):
     except:
         return 'error\n'
 def ls-l(path):
+
+
     try:
         path  = os.listdir(os.getcwd())
         for name in path:
             files  = os.path.join(os.getcwd(), name)
             inode = os.stat(files)
-            #inf=str(inode.st_mode)
-            #inf=str(stat.filemode(os.stat(files).st_mode))
             inf=str(inode.st_mode)
-            gid=str(inode.st_gid)
-            uid=str(inode.st_uid)
+            #inf=str(stat.filemode(os.stat(files).st_mode)
+
+            gid=str(g/home/sun/ls-l.pyetgrgid(inode.st_gid).gr_name)
+            uid=str(getpwuid(inode.st_uid).pw_name)
             siz=str(inode.st_size)
-            time=str(inode.st_atime)
-            #date=datetime.datetime.fromtimestamp(time)
-            print(inf + ' ' + gid + ' ' + uid + ' '  + time +' '+ siz  + ' ' + name)
+            #time=str(inode.st_atime)
+            date=datetime.utcfromtimestamp(inode.st_atime).strftime('%Y-%m-%d %H:%M:%S')
+            #date=time.ctime(int(time))
+            #date=datetime.datetime.fromtimestamp(int(time))
+            permission ={'0':('---'),'1':('--x'),'2':('-w-'),'3':('-wx'),'4':('r--'),'5':('rw-'),'6':('rw-'),'7':('rwx')}
+            #inf=str(permission(inode.st_mode))
+
+            print(inf + ' ' + gid + ' ' + uid + ' '  + date +' '+ siz  + ' ' + name)
 
     except:
         return 'error\n'
+
 
 def ls():
     try:

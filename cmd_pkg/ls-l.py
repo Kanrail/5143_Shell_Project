18ff0705301deaaa4530158f7a9cf6832731825e
Shenglin Sun <sunshenglin12@gmail.com>
2019-02-14 20:33:27 -0600

    Add files via upload

--- cmd_pkg/ls-l.py
+++ cmd_pkg/ls-l.py
@@ -5,28 +5,29 @@ import time
 from grp import getgrgid
 from pwd import getpwuid
 def ls():
-    try:
+    #try:
         path  = os.listdir(os.getcwd())
         for name in path:
             files  = os.path.join(os.getcwd(), name)
             inode = os.stat(files)
-            inf=str(inode.st_mode)
+            #inf=str(inode.st_mode)
             #inf=str(stat.filemode(os.stat(files).st_mode)
-           
+                   
             gid=str(getgrgid(inode.st_gid).gr_name)
             uid=str(getpwuid(inode.st_uid).pw_name)
             siz=str(inode.st_size)
-            time=str(inode.st_atime)
-            #date=datetime.utcfromtimestamp(time).strftime('%Y-%m_d %H:%%M:%S')
-            #date=time.ctime(int(time))
-            #date=datetime.datetime.fromtimestamp(int(time))
-            permission ={'0':('---'),'1':('--x'),'2':('-w-'),'3':('-wx'),'4':('r--'),'5':('rw-'),'7':('rwx')}
-            #inf=str(permission(inode.st_mode))
+            
+            b=''
+            date=datetime.utcfromtimestamp(inode.st_atime).strftime('%Y-%m-%d %H:%M:%S')
+            permission ={'8':'---','1':'--x','2':'-w-','3':'-wx','4':'r--','5':'rw-','6':'rw-','7':'rwx'}
+            inf=str(inode.st_mode)
+            for i in inf:
+                b=b+permission[i]
 
-            print(inf + ' ' + gid + ' ' + uid + ' '  + time +' '+ siz  + ' ' + name)
+            print(b + ' ' + gid + ' ' + uid + ' '  + date +' '+ siz  + ' ' + name)
 
-    except:
-        return 'error\n'
+    #except:
+     #   return 'error\n'
 
 if __name__ == '__main__':
     a=ls()
